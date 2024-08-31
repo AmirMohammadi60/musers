5d581a3293ef22ba646c7abebb67b2b89cf1854d
mohammadi-amir <amirmm@web.de>
2024-08-31 14:07:06 +0200

    mergen

--- main.py
+++ main.py
@@ -12,40 +12,32 @@ with open(file_path, 'rb') as f:
     while True:
         data = f.read(utmp_size) # iest eine bestimmte Anzahl von Bytes aus einer Datei in die Variable data ein.
         if not data:
-            break
-       entry = struct.unpack(utmp_format, data) # binären Daten in der Variable data in ein Python-Tupel zu entpacken, basierend auf dem Formatstring utmp_format
-      ##############################################################################################################################################################################################
-        try:                                                                                                                                                                                       #
-      ##############################################################                                                                                                                               #
-            user = entry[4].decode('UTF8').strip('\x00')           #                                                                                                                               #
-            terminal = entry[2].decode('latin-1').strip('\x00')    # extrahieren und dekodieren spezifische Felder aus dem zuvor entpackten entry-Tupel und bereiten sie als lesbare Strings auf   #
-            host = entry[5].decode('latin-1').strip('\x00')        #                                                                                                                               #
-      ##############################################################                                                                                                                               # Error handling    
-        except UnicodeDecodeError:                                                                                                                                                                 #
-      ##############################################################################                                                                                                               #   
-            user = entry[4].decode('latin-1', errors='replace').strip('\x00')      #                                                                                                               #
-            terminal = entry[2].decode('latin-1', errors='replace').strip('\x00')  # wie mit Fehlern während der Dekodierung umgegangen werden soll.                                               #
-            host = entry[5].decode('latin-1', errors='replace').strip('\x00')      #                                                                                                               #
-      ##############################################################################                                                                                                               #     
-      ##############################################################################################################################################################################################                                                                                                                                                                                             
+         break
+        entry = struct.unpack(utmp_format, data) # binären Daten in der Variable data in ein Python-Tupel zu entpacken, basierend auf dem Formatstring utmp_format
+      ################################################################################################################################################################################################
+        try:                                                                                                                                                                                        ##
+      ###############################################################                                                                                                                               ##
+            user = entry[4].decode('UTF8').strip('\x00')           ##                                                                                                                               #
+            terminal = entry[2].decode('latin-1').strip('\x00')    ## extrahieren und dekodieren spezifische Felder aus dem zuvor entpackten entry-Tupel und bereiten sie als lesbare Strings auf## ##
+            host = entry[5].decode('latin-1').strip('\x00')        ##                                                                                                                               ##
+      ###############################################################                                                                                                                               ## Error handling
+        except UnicodeDecodeError:                                                                                                                                                                  ##
+      ###############################################################################                                                                                                               ##
+            user = entry[4].decode('latin-1', errors='replace').strip('\x00')      ##                                                                                                               ##
+            terminal = entry[2].decode('latin-1', errors='replace').strip('\x00')  ## wie mit Fehlern während der Dekodierung umgegangen werden soll.# #                                            ##
+            host = entry[5].decode('latin-1', errors='replace').strip('\x00')      ##                                                                                                               ##
+      ###############################################################################                                                                                                               ##
+      ################################################################################################################################################################################################
        
         timestamp = entry[8] # Hier wird angenommen, dass entry eine Liste oder ein Tupel ist, und das 9. Element (entry[8], da Indexe bei 0 beginnen) enthält den Unix-Zeitstempel                
         Time = time.ctime(timestamp) # wandelt einen Unix-Zeitstempel in eine lesbare Zeichenkette um, die das Datum und die Uhrzeit darstellt.
 
 
-      ###########################################################################
-        if user and user not in ["runlevel", "reboot"]:                         #
-         if host and host not in ["login screen"]:                              # überprüft Bedingungen für Variablen user und host, und wenn diese Bedingungen erfüllt sind, wird eine formatierte Ausgabe erzeugt. Hier ist eine detaillierte Erklärung:                                                                   
-             print(f"{user:<10} {host:<12} {terminal:<10} {Time}")              #
-      ###########################################################################       
-
-
-
-
-
-
-
-
+       ###########################################################################
+        if user and user not in ["runlevel", "reboot"]:                         ##
+         if host and host not in ["login screen"]:                              ## überprüft Bedingungen für Variablen user und host, und wenn diese Bedingungen erfüllt sind, wird eine formatierte Ausgabe erzeugt. Hier ist eine detaillierte Erklärung:
+             print(f"{user:<10} {host:<12} {terminal:<10} {Time}")              ##
+       ###########################################################################
 
 
 
