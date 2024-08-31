0ac4812a41e23431fc7646856744eb7819409ff4
mohammadi-amir <amirmm@web.de>
2024-08-31 15:15:49 +0200

    ##

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import struct # um zwischen Python-Werten und Byte-Objekten zu konvertieren, also um Daten zu packen und zu entpacken. Insbesondere ist struct nützlich, wenn man mit binären Datenformaten arbeiten muss
-import time   # Man muss time importieren, wenn man mit Zeitstempeln, Verzögerungen oder der Messung von Zeitintervallen arbeiten möchte.
+import struct ### um zwischen Python-Werten und Byte-Objekten zu konvertieren, also um Daten zu packen und zu entpacken. Insbesondere ist struct nützlich, wenn man mit binären Datenformaten arbeiten muss
+import time   ### Man muss time importieren, wenn man mit Zeitstempeln, Verzögerungen oder der Messung von Zeitintervallen arbeiten möchte.
 
 
-utmp_format = 'hi32s4s32s256shhiii4i20s' # definiert das Format eines Datenblocks.
-utmp_size = struct.calcsize(utmp_format) # berechnet die Größe (in Bytes) der Datenstruktur, die durch den Formatstring utmp_format beschrieben wird
+utmp_format = 'hi32s4s32s256shhiii4i20s' ### definiert das Format eines Datenblocks.
+utmp_size = struct.calcsize(utmp_format) ### berechnet die Größe (in Bytes) der Datenstruktur, die durch den Formatstring utmp_format beschrieben wird
 
 file_path = '/var/run/utmp' 
 
 with open(file_path, 'rb') as f:
     print(f"{'User':<10} {'Host':<10} {'Terminal':<10} {'Time'}")
     while True:
-        data = f.read(utmp_size) # iest eine bestimmte Anzahl von Bytes aus einer Datei in die Variable data ein.
+        data = f.read(utmp_size) ### iest eine bestimmte Anzahl von Bytes aus einer Datei in die Variable data ein.
         if not data:
          break
-        entry = struct.unpack(utmp_format, data) # binären Daten in der Variable data in ein Python-Tupel zu entpacken, basierend auf dem Formatstring utmp_format
+        entry = struct.unpack(utmp_format, data) ### binären Daten in der Variable data in ein Python-Tupel zu entpacken, basierend auf dem Formatstring utmp_format
       ################################################################################################################################################################################################
         try:                                                                                                                                                                                        ##
       ###############################################################                                                                                                                               ##
